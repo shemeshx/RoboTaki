70d6b8d6bfdbcdaec2fd8c6dff8f14a5bb436c13
Ron <Ron@Ron-LAP>
2016-04-30 13:58:03 +0300

    some fixes in findAvailableCard

--- Test/src/main.java
+++ Test/src/main.java
@@ -4,6 +4,7 @@ import Cards.Card;
 import Cards.Colors;
 import Cards.cardType;
 import Cards.numberCard;
+import Cards.specialCardColor;
 import Cards.specialCardNoColor;
 import Game.RobotTaki;
 import Game.deckHand;
@@ -11,9 +12,9 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String []str={"8 BLUE","SWITCH RED","2 GREEN","COLOR","SUPER","TAKI RED","5 RED","9 RED"};
+		String []str={"8 BLUE","SWITCH RED","2 GREEN","COLOR","SUPER","TAKI RED","PLUS RED","9 RED"};
 		RobotTaki r=new RobotTaki(str);
-		r.playTurn(new specialCardNoColor(cardType.COLOR));
+		r.playTurn(new specialCardColor(cardType.PLUS,Colors.BLUE));
 		
 	}
 
